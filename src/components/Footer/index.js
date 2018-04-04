5fb3d0be50d06ddb889d2a6bc9924535db06397e
Brandon Smith <brandon@reddirtwebdesign.com>
2018-04-04 16:38:16 -0700

    Removed Extra Closing Tag From Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,7 +10,7 @@ const Footer = () => (
     <footer>
       <p className="copy">&copy; 2018 RoadRunner Restaurant &amp; Saloon</p>
       <a href="http://www.roadrunnerrestaurantandsaloon.com/" target="_blank">
-        <img className="rr" src={RRLogo} alt="Road Runner Logo" />>
+        <img className="rr" src={RRLogo} alt="Road Runner Logo" />
       </a>
       <a href="https://Wheretheybuck.com" target="_blank">
         <img className="wtb" src={WTBLogo} alt="Where They Buck Logo" />
